96c812a4a36b97b596dfef7eac232c81b67619d9
Kumar Abhinav <kumarabhinav752001@gmail.com>
2025-11-09 19:49:31 +0530

    MongoDB connected

---
+++ src/db/database_config.db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+    try{
+        const connectToInstance = await mongoose.connect(`${process.env.MONGO_UR}`);
+        console.log(`MONGODB CONNECTED AT THE HOST: ${connectToInstance.connection.host}`);
+
+    }catch(error){
+        console.log('MONGODB CONNECTION FAILED:', error);
+        process.exit(1);
+    }
+}
+
+export default connectToDB
